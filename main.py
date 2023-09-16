bfb980f758fdcb52aaa8c3d2d930a9cd6fd6523c
bini¿?!! <140443541+binic0des@users.noreply.github.com>
2023-09-15 22:48:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# 1
+perimetro = float(input("Introduzca la longitud del lado del cuadrado: "))
+perimetro *= 4
+
+print(perimetro)
+
+
+# 2
+numeros = [57, 45, 14, 93, 8, 10]
+maximo = max(numeros)
+minimo = min(numeros)
+
+print("El número máximo de la lista es:", maximo)
+print("El número mínimo de la lista es:", minimo)
+
+
+# 10
+num_ascendentes = [25, 58, 47, 87, 12, 98]
+num_ascendentes.sort()
+
+print("Los numeros ordenados de forma ascendetes son: ", num_ascendentes)
+
+
+# 5
+lista_num = [1,2,3,4,5,6,7,8]
+lista_num.reverse()
+
+print(lista_num)
+
